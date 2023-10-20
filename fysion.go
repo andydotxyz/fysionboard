ae753ad18d798120fa2c656e14d5866351648266
Andy Williams <andy@andy.xyz>
2023-10-20 15:25:20 +0100

    Save content in preferences

--- fysion.go
+++ fysion.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"io"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"fyne.io/fyne/v2"
@@ -23,11 +24,43 @@ type board struct {
 
 type fysion struct {
 	top *board
+	app fyne.App
 
 	title *widget.RichText
 	body  *fyne.Container
 
 	win fyne.Window
+}
+
+func (f *fysion) addColor(c color.Color) {
+	r := canvas.NewRectangle(c)
+	r.SetMinSize(fyne.NewSize(150, 50))
+	f.body.Add(r)
+}
+
+func (f *fysion) addFile(name string, r io.ReadCloser) {
+	defer r.Close()
+	b, _ := io.ReadAll(r)
+
+	switch strings.ToLower(filepath.Ext(name)) {
+	case ".png", ".jpeg", ".jpg":
+		res := fyne.NewStaticResource(name, b)
+		add := canvas.NewImageFromResource(res)
+		add.FillMode = canvas.ImageFillContain
+		add.ScaleMode = canvas.ImageScaleFastest
+		f.body.Add(add)
+	case ".txt":
+		txt := widget.NewLabel(string(b))
+		txt.Wrapping = fyne.TextWrapWord
+		f.body.Add(txt)
+	default:
+		dialog.ShowError(errors.New("unsupported file type "+filepath.Ext(name)), f.win)
+	}
+}
+
+func (f *fysion) addText(text string) {
+	r := widget.NewRichTextFromMarkdown("## " + text)
+	f.body.Add(r)
 }
 
 func (f *fysion) buildUI() fyne.CanvasObject {
@@ -50,6 +83,7 @@ func (f *fysion) buildUI() fyne.CanvasObject {
 					return
 				}
 				f.title.ParseMarkdown("# " + input.Text)
+				f.app.Preferences().SetString(boardID+".name", input.Text)
 			}, f.win)
 	})
 	top := container.NewBorder(nil, nil, nil, container.NewHBox(edit, add),
@@ -57,6 +91,33 @@ func (f *fysion) buildUI() fyne.CanvasObject {
 
 	return container.NewBorder(top, nil, nil, nil,
 		container.NewVScroll(f.body))
+}
+
+func (f *fysion) setBoard(title string, items []string) {
+	f.title.ParseMarkdown("# " + title)
+	f.body.RemoveAll()
+	for _, item := range items {
+		u, err := storage.ParseURI(item)
+		if err != nil {
+			f.addText(item)
+			continue
+		}
+
+		switch u.Scheme() {
+		case "file":
+			r, err := storage.Reader(u)
+			if err != nil {
+				fyne.LogError("Failed to open reader", err)
+				continue
+			}
+			f.addFile(u.Name(), r)
+		case "color":
+			c := parseColor(item[8:]) // strip the color://
+			f.addColor(c)
+		default:
+			f.addText(item)
+		}
+	}
 }
 
 func (f *fysion) showAdd(p fyne.Position) {
@@ -73,9 +134,10 @@ func (f *fysion) showAddColor() {
 			if c == nil {
 				return
 			}
-			r := canvas.NewRectangle(c)
-			r.SetMinSize(fyne.NewSize(150, 50))
-			f.body.Add(r)
+			f.addColor(c)
+			list := f.app.Preferences().StringList(boardID + ".items")
+			list = append(list, "color://"+formatColor(c))
+			f.app.Preferences().SetStringList(boardID+".items", list)
 		}, f.win)
 }
 
@@ -89,24 +151,10 @@ func (f *fysion) showAddFile() {
 			return
 		}
 
-		defer r.Close()
-		b, err := io.ReadAll(r)
-		name := r.URI().Name()
-
-		switch strings.ToLower(filepath.Ext(name)) {
-		case ".png", ".jpeg", ".jpg":
-			res := fyne.NewStaticResource(name, b)
-			add := canvas.NewImageFromResource(res)
-			add.FillMode = canvas.ImageFillContain
-			add.ScaleMode = canvas.ImageScaleFastest
-			f.body.Add(add)
-		case ".txt":
-			txt := widget.NewLabel(string(b))
-			txt.Wrapping = fyne.TextWrapWord
-			f.body.Add(txt)
-		default:
-			dialog.ShowError(errors.New("unsupported file type "+filepath.Ext(name)), f.win)
-		}
+		f.addFile(r.URI().Name(), r)
+		list := f.app.Preferences().StringList(boardID + ".items")
+		list = append(list, r.URI().String())
+		f.app.Preferences().SetStringList(boardID+".items", list)
 	}, f.win)
 	o.SetFilter(storage.NewExtensionFileFilter([]string{".png", ".jpeg", ".jpg", ".txt"}))
 	o.Show()
@@ -122,7 +170,33 @@ func (f *fysion) showAddText() {
 			if !ok {
 				return
 			}
-			r := widget.NewRichTextFromMarkdown("## " + input.Text)
-			f.body.Add(r)
+			f.addText(input.Text)
+			list := f.app.Preferences().StringList(boardID + ".items")
+			list = append(list, input.Text)
+			f.app.Preferences().SetStringList(boardID+".items", list)
 		}, f.win)
+}
+
+func parseColor(s string) color.Color {
+	parts := strings.Split(s, ",")
+	if len(parts) != 4 {
+		return color.Transparent
+	}
+
+	r, _ := strconv.Atoi(parts[0])
+	g, _ := strconv.Atoi(parts[1])
+	b, _ := strconv.Atoi(parts[2])
+	a, _ := strconv.Atoi(parts[3])
+	return color.NRGBA64{R: uint16(r), G: uint16(g), B: uint16(b), A: uint16(a)}
+}
+
+func formatColor(c color.Color) string {
+	r, g, b, a := c.RGBA()
+
+	parts := make([]string, 4)
+	parts[0] = strconv.Itoa(int(r))
+	parts[1] = strconv.Itoa(int(g))
+	parts[2] = strconv.Itoa(int(b))
+	parts[3] = strconv.Itoa(int(a))
+	return strings.Join(parts, ",")
 }
--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
 	"fyne.io/fyne/v2/app"
 )
 
+const boardID = "default"
+
 func main() {
 	a := app.NewWithID("xyz.andy.fysionboard")
 	w := a.NewWindow("FysionBoard")
 
-	f := &fysion{win: w}
+	f := &fysion{app: a, win: w}
 	w.SetContent(f.buildUI())
 	w.Resize(fyne.NewSize(minColWidth*1.5, minColWidth*2.5))
+
+	title := a.Preferences().String(boardID + ".name")
+	if title != "" {
+		content := a.Preferences().StringList(boardID + ".items")
+		f.setBoard(title, content)
+	}
 	w.ShowAndRun()
 }
